84d6dad584b7b75c2cec9f7d98068dcc02e84654
Parthib <parthibbanerjee01@gmail.com>
2025-08-18 20:47:18 +0530

    Implemented a simple chain in langchain

---
+++ Langchain_Chains/simple_chain.py
@@ -0,0 +1,22 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+result = chain.invoke({'topic':'graphrag'})
+
+print(result)
+
+chain.get_graph.print_ascii()
